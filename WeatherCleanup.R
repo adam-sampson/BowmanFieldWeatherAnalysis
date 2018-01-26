8102189b5c929eab0bd28307d3950eaec0d34437
Adam <12676475+adam-sampson@users.noreply.github.com>
2018-01-26 16:29:41 -0500

    Adding two new R files
    WeatherCleanup.R is intended to replace * missing values with R friendly NA.
    dataInfo.R allows the user to more easily bring up the meaning of each variable.

---
+++ WeatherCleanup.R
@@ -0,0 +1,46 @@
+##########
+## Cleanup weather data
+##########
+
+#---
+# Load Packages
+#---
+  packages <- c("tidyverse", "lubridate", "xts", "stringr")
+  new_packages <- packages[!(packages %in% installed.packages()[,"Package"])]
+  if(length(new_packages)) install.packages(new_packages)
+  #   Load Neccessary Packages
+  sapply(packages, require, character.only = TRUE)
+  rm(new_packages)
+    
+#---
+# Replace missing values
+#---
+  clean.df <- df
+  for(col in names(clean.df)) {
+    clean.df[,col] <- str_replace_all(clean.df[,col],"^[*]+$","NA")
+    clean.df[clean.df[,col]=="NA",col] <- NA
+  }
+  
+#---
+# Generate stats about variables
+*---
+  clean.df.stats <- list()
+  
+  ## Determine what percent of a variable is NA
+  clean.df.stats[["na_percent"]] <- list()
+  for(col in names(clean.df)) {
+    clean.df.stats[["na_percent"]][col] <- 100*sum(is.na(clean.df[,col]))/length(clean.df[,col])
+  }
+  clean.df.stats$na_percent
+  
+#---
+# Fix variable types
+#---
+  clean.df$DATE_TIME <- ymd_hms(clean.df$DATE_TIME)
+  
+  numeric.var <- c("DIR","SPD","GUS","CLG","VSB","TEMP","DEWP","ALT")
+  for(col in numeric.var) {
+    clean.df[col] <- as.numeric(unlist(clean.df[col]))
+  }
+  rm(numeric.var)
+  
